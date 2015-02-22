7fffea7384c860c007321c516abc94354aab0890
Honza Novak <kacerr.cz@gmail.com>
2015-02-22 16:42:10 +0100

    create user<-->book relation, create dashboard view

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :books
 
 
-  get "/dashboard" => "dashboard#index", as: :dashboard
+  get "dashboard" => "pages#dashboard", as: :dashboard
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
