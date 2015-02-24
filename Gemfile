afdb60d6ecc775303c9d417dcb14aa6fb45c88de
Honza Novak <kacerr.cz@gmail.com>
2015-02-24 14:56:05 +0100

    Implement stripe payment.

--- Gemfile
+++ Gemfile
@@ -32,9 +32,10 @@ gem 'devise', '~> 3.4.1'
 gem 'cancancan', '~> 1.10.1'
 gem 'paperclip', '~> 4.2.1'
 gem 'friendly_id', '~> 5.1.0'
+gem 'stripe', '~> 1.20.0'
 
 # i want dotenv everywhere is i might be using that on my own production server
-gem 'dotenv', '~> 1.0.2'
+gem 'dotenv-rails', '~> 1.0.2'
 
 group :development do 
   gem 'quiet_assets', '~> 1.1.0'
--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,11 @@ Rails.application.routes.draw do
 
   resources :books
 
-
   get "dashboard" => "pages#dashboard", as: :dashboard
+
+  post '/buy/:slug', to: 'transactions#create', as: :buy
+  get '/pickup/:guid', to: 'transactions#pickup', as: :pickup
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
